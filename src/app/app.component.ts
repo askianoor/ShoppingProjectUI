5b31e547f996148a5433a8e40df8e8100d19ec08
Ali Kianoor <askianoor@gmail.com>
2019-10-19 20:37:53 +0330

    Add products API view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Shopping';
+  products: object;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:56283/api/Products').subscribe(data => {
+      this.products = data;
+    });
+  }
 }
